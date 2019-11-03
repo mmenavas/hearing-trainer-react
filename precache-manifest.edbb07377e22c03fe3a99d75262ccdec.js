e3f4ae5fbfc663136e8bfb5a559a9f9e2226bdb5
Maximo Mena <maximo.mena@semanticbits.com>
2019-11-03 01:04:39 -0700

    Updates

---
+++ precache-manifest.edbb07377e22c03fe3a99d75262ccdec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ff62f6bd55c4b865bb5c5549e7e98f",
+    "url": "/hearing-trainer/index.html"
+  },
+  {
+    "revision": "404ae38a7a99d6152c74",
+    "url": "/hearing-trainer/static/css/main.0f7f30a2.chunk.css"
+  },
+  {
+    "revision": "6401aa3e4bb78aed1819",
+    "url": "/hearing-trainer/static/js/2.bb22fd3a.chunk.js"
+  },
+  {
+    "revision": "404ae38a7a99d6152c74",
+    "url": "/hearing-trainer/static/js/main.96f322d4.chunk.js"
+  },
+  {
+    "revision": "68861e23dca3a3abfd20",
+    "url": "/hearing-trainer/static/js/runtime-main.f0c967aa.js"
+  }
+]);
