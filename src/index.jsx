841871db1893695f00044b0a4ee0b974d74cecfc
Maximo Mena <maximo.mena@lullabot.com>
2025-12-28 00:33:23 -0700

    Upgrade react to v19

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import 'normalize.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -16,7 +16,9 @@ const notes = [
   'C5',
 ]
 
-ReactDOM.render(<App notes={notes} />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+  <App notes={notes} />
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
